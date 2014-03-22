cfbc6e624834eb586f49a5cce5365d3451f04aee
Simon Lomax <simax.99.99@gmail.com>
2014-03-22 17:25:19 +0000

    Changed project.clj to use clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tutorial "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [liberator "0.10.0"]
                  [compojure "1.1.3"]
                  [ring/ring-core "1.2.1"]
